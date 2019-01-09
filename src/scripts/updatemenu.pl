1202783a265bd2b3edd329dc948a7014a302b16b
zhangkaiheb <zhangkaiheb@126.com>
2019-01-09 07:11:17 -0800

    build third level menu list

--- src/scripts/updatemenu.pl
+++ src/scripts/updatemenu.pl
@@ -50,8 +50,13 @@ my @sublines = ();
 my $line;
 my %menunumbers = ();
 
+my @l3lines = ();
+#my %menul2numbers = ();
+my %menul3numbers = ();
+
 @lines = `cat /var/ofw/main/menu.lst`;
 @sublines = `grep "# MENUENTRY" ${path}*.cgi`;
+@l3lines = `grep "# MENUTHRDLVL" ${path}*.cgi`;
 # TODO: die if no content
 
 open(MENUFILE, ">/usr/lib/ofw/menu.pl") or die "Unable to write menu";
@@ -67,6 +72,7 @@ print MENUFILE <<END
 
 package Menu;
 \%Menu::menu = ();
+\%Menu::l3menu = ();
 
 sub buildmenu()
 {
@@ -127,11 +133,23 @@ foreach $line (sort @sublines) {
     my $sub = $3;
     $line = $4;
 
+#    next unless ($line =~ /"(.+?)"\s*(.*)/);
+#    my $submenu = "'$1'";
+
     if (!defined($menunumbers{$name})) {
         # protect against non existing top-level
         system("/usr/bin/logger -t openfirewall Missing MENUTOP ($name) for $cginame");
         next;
     }
+
+#    if (defined($menul2numbers{$submenu})) {
+#        system("/usr/bin/logger -t openfirewall Duplicate MENUENTRY: $submenu");
+#        open(STDERR, '>&', STDOUT) or die "Can't redirect stderr: $!";
+#        print "p1 $1, name $name, sub $sub, line $line\n";
+#        next;
+#    }
+#    $menul2numbers{$submenu} = "1";
+
     push(@lines, "$menunumbers{$name} $sub $cginame $line\n");
 }
 
@@ -191,6 +209,117 @@ END
 
     print MENUFILE "\n";
 }
+
+
+##########################################################################################
+#                                                                                        #
+# Fetch third-level entries first.                                                       #
+#                                                                                        #
+##########################################################################################
+
+foreach $line (@l3lines) {
+    chomp $line;
+    next unless ($line =~ /${path}(.*):# MENUTHRDLVL\s+"(.+?)"\s+(\d{3})\s+(.*)/);
+    my $head = $1;
+    my $name = $2;
+    my $contents = "\$Lang::tr{'$1'}";
+    # Revert to the 'real text' if translation is missing
+    $contents = "'$1'" unless(defined($Lang::tr{$1}));
+
+#    if (!defined($menul2numbers{$name})) {
+#        # protect against duplicates
+#        system("/usr/bin/logger -t openfirewall Missing MENUENTRY: $name");
+#        next;
+#    }
+
+
+    if (defined($menul3numbers{$name})) {
+        system("/usr/bin/logger -t openfirewall Duplicate MENUENTRY: $name");
+        next;
+    }
+    # TODO: skip top-level if there are no matching sub-level entries
+    #  If a sub-level is missing, menu creation will fail in header.pl
+
+    $menul3numbers{$name} = "1";
+
+    # And print to menu.pl
+    print MENUFILE <<END
+    \%{\$Menu::l3menu{"$name"}} = (
+        'subMenu'    => []
+    );
+END
+    ;
+}
+
+print MENUFILE "\n";
+
+
+#
+# Write out third-level entries, sort on numbers.
+#
+@lines = ();
+foreach $line (sort @l3lines) {
+    chomp $line;
+    next unless ($line =~ /${path}(.*):# MENUTHRDLVL\s+"(.+?)"\s+(\d{3})\s+(.*)/);
+    my $cginame = "/cgi-bin/$1";
+    my $name = $2;
+    my $sub = $3;
+    $line = $4;
+
+#    if (!defined($menul2numbers{$name})) {
+#        # protect against non existing top-level
+#        system("/usr/bin/logger -t openfirewall Missing MENUENTRY ($name) for $cginame");
+#        next;
+#    }
+    push(@lines, "$name $sub $cginame $line\n");
+}
+
+#
+# We now have a sortable list
+#
+
+foreach $line (sort @lines) {
+    chomp $line;
+    next unless ($line =~ /(.+?)\s+(\d{3})\s+(.*?)\s+(.*)/);
+    my $sub = $2;
+    my $cginame = "$3";
+    $line = $4;
+
+    # find the sub-level menu
+    my $head = "\$Lang::tr{'$1'}";
+    $head = "'$1'" unless(defined($Lang::tr{$1}));
+
+    # We need at least 1 text
+    next unless ($line =~ /"(.+?)"\s*(.*)/);
+    my $contents = "\$Lang::tr{'$1'}";
+    $contents = "'$1'" unless(defined($Lang::tr{$1}));
+    $line = $2;
+
+    # A 2nd text is optional
+    my $status = "''";
+    if ($line =~ /"(.*?)"\s*(.*)/) {
+        $status = "\$Lang::tr{'$1'}";
+        $status = "'$1'" unless(defined($Lang::tr{$1}));
+        $line = $2;
+    }
+
+    # A 3rd text is also optional
+    my $uri = "";
+    if ($line =~ /"(.*?)"\s*(.*)/) {
+        $cginame = "${cginame}$1";
+        $line = $2;
+    }
+
+    # And print to menu.pl
+    print MENUFILE <<END
+    push(\@{\$Menu::l3menu{"$head"}{'subMenu'}}, [ $contents, '$cginame', $status ]);
+END
+    ;
+
+    print MENUFILE "\n";
+}
+
+
 
 print MENUFILE <<END
 }
